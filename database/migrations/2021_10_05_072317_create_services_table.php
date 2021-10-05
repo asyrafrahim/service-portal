42b7a22109b8428f82b91b7ce04766333559d147
Asyraf Rahim <asyrafboyz@gmail.com>
2021-10-05 16:52:18 +0800

    migrate model spatie

--- database/migrations/2021_10_05_072317_create_services_table.php
+++ database/migrations/2021_10_05_072317_create_services_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateServicesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('services', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('contact');
-            $table->rememberToken();
+            $table->string('title');
+            $table->text('description');
+            $table->string('attachment_1');
+            $table->string('attachment_2');
             $table->timestamps();
         });
     }
@@ -32,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('services');
     }
 }
